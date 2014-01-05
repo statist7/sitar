a016c35487837e9f3022d645f9f9ad8f86074980
Tim Cole <tim.cole@ucl.ac.uk>
2014-01-05 17:26:39 +0000

    add BICadj AICadj rm devadj

--- R/sitarlib.R
+++ R/sitarlib.R
@@ -633,48 +633,6 @@
 		
 #############################
 #
-#	devadj
-#
-#############################
-
-	devadj <- function(..., pattern=NULL)
-#	input SITAR model(s)
-#	returns deviance for power transformed y
-#	looks for log, sqrt or ^ - assumes no multipliers
-{	ARG <- list(...)
-	if (!is.null(pattern)) {
-		pattern <- ls(envir=parent.frame(), pattern=pattern)
-		ARG <- c(ARG, lapply(as.list(pattern), get))
-	}
-	dev <- lapply(ARG, function(obj) {
-		if (!'call' %in% names(obj)) return(NA)
-#	check for call.sitar$y or else call$y or else call$formula
-		if (!is.null(obj$call.sitar)) obj$call <- obj$call.sitar
-#	check for call$y or call$model
-		if (!is.null(obj$call$y)) ycall <- obj$call$y 
-			else if (!is.null(obj$call$model)) ycall <- obj$call$model[[2]] 
-			else if (!is.null(obj$call$formula)) ycall <- obj$call$formula[[2]] 
-				else return(NA)
-		lambda <- 1
-		if (length(ycall) == 1) yt <- ycall else {
-			yt <- ycall[[2]]
-			fun <- ycall[1]
-			if (fun == "log()") lambda <- 0 else
-			if (fun == "sqrt()") lambda <- 0.5 else 
-			if (fun == "`^`()") lambda <- eval(ycall[[3]])
-		}
-		y <- eval(yt, eval(obj$call$data, sys.parent()))
-		sly <- ifelse(lambda == 1, 0, sum(log(y)))
-		-2 * (obj$logLik + (lambda-1) * sly + length(y) * log(abs(lambda) + (lambda == 0)))		
-	})
-	names(dev) <- c(match.call(expand.dots=FALSE)$..., pattern)
-	dev <- unlist(dev[!is.na(dev)])
-	if (length(dev) == 0) return(invisible())
-	dev[order(dev)]
-}
-
-#############################
-#
 #	varexp
 #
 #############################
