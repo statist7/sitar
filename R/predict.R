ed313311b6a5e58bb30ed340683c3e27724977dc
Tim Cole <tim.cole@ucl.ac.uk>
2015-03-24 17:27:24 +0000

    predict and .fitnlme
    if .fitnlme missing from object, recreate using update(returnsub=TRUE)

--- R/predict.R
+++ R/predict.R
@@ -1,7 +1,7 @@
   predict.sitar <- function(object, newdata, level, ...) {
     mcall <- match.call()[-1]
     if (!missing(newdata)) {
-      on.exit(rm(.fitnlme, envir=globalenv()))
+      on.exit(if (exists('.fitnlme')) rm(.fitnlme, envir=globalenv()))
       scall <- object$call.sitar
       xname <- deparse(scall$x)
       if (!xname %in% names(newdata)) stop('newdata lacks x variable')
@@ -16,7 +16,8 @@
       fe <- fe[!match(names(fe), names(newdata), 0)]
       newdata <- data.frame(newdata, t(fe))
       mcall$newdata <- quote(newdata)
-      .fitnlme <<- object$.fitnlme
+      if (exists('object$.fitnlme')) .fitnlme <<- object$.fitnlme else
+        .fitnlme <<- update(object, returnsub=TRUE)
     }
     do.call(nlme:::predict.nlme, as.list(mcall))
   }
