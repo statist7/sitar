ee452d6c06390c00d35675504eecfd965cf1fe71
Tim Cole <tim.cole@ucl.ac.uk>
2015-06-15 17:29:35 +0100

    xyadj simplify factor(id)

--- R/xyadj.R
+++ R/xyadj.R
@@ -7,17 +7,17 @@ xyadj <- function(object) {
   if (!is.null(subset)) data <- data[subset,]
   x <- eval(mcall$x, data)
   y <- eval(mcall$y, data)
-  id <- eval(mcall$id, data)
+  id <- factor(eval(mcall$id, data))
   nf <- length(fitted(object))
   if (nf != length(y)) stop(paste('model (length=', nf, ') incompatible with data (rows=', length(y), ')', sep=''))
   xoffset <- object$xoffset
   if (is.null(xoffset)) xoffset <- 0
   if (!is.na(fixef(object)['b'])) xoffset <- xoffset + fixef(object)['b']
   x.adj <- x - xoffset
-  if (grepl('b', random)) x.adj <- x.adj - ranef(object)[factor(id),'b']
-  if (grepl('c', random)) x.adj <- x.adj * exp(ranef(object)[factor(id),'c'])
+  if (grepl('b', random)) x.adj <- x.adj - ranef(object)[id,'b']
+  if (grepl('c', random)) x.adj <- x.adj * exp(ranef(object)[id,'c'])
   x.adj <- x.adj + xoffset
   y.adj <- y
-  if (grepl('a', random)) y.adj <- y.adj - ranef(object)[factor(id),'a']
+  if (grepl('a', random)) y.adj <- y.adj - ranef(object)[id,'a']
   invisible(data.frame(x=x.adj, y=y.adj))
 }
