496c2c8c08cdf5cc71d7f96b6b0ac7eb14e9c9a5
Tim Cole <tim.cole@ucl.ac.uk>
2015-06-17 15:10:15 +0100

    predict.sitar add deriv arg

--- R/predict.R
+++ R/predict.R
@@ -1,35 +1,41 @@
-  predict.sitar <- function(object, newdata, level=1, ...) {
-    mcall <- match.call()[-1]
-    if (!missing(newdata)) {
-      if (!'x' %in% names(newdata)) stop('newdata lacks x variable')
-      if (!'id' %in% names(newdata)) {
-        if (level != 0) stop('newdata lacks id variable')
-        newdata <- data.frame(newdata, id=object$groups$id[1])
-      }
+  predict.sitar <- function(object, newdata, level=1, ..., deriv=0, xfun=I, yfun=I) {
+# create x and id variables in newdata
+    oc <- object$call.sitar
+    if (missing(newdata)) newdata <- eval(oc$data)
+    newdata$x <- x <- eval(oc$x, newdata)
+    newdata$id <- id <- if (level == 1) eval(oc$id, newdata) else
+      rep.int(getGroups(object)[1], nrow(newdata))
 # attach object for fitnlme
-      on.exit(detach(object))
-      eval(parse(text='attach(object)'))
-      if (!exists('fitnlme'))
-        stop('could not find function "fitnlme": please update model')
+    on.exit(detach(object))
+    eval(parse(text='attach(object)'))
+    if (!exists('fitnlme'))
+      stop('could not find function "fitnlme": please update model')
 # omit fitnlme args already in newdata
-  		argnames <- names(formals(fitnlme))
-			args <- setNames(vector('integer', length=length(argnames)), argnames)
-      args <- args[!match(argnames, names(newdata), 0)]
-      newdata <- data.frame(newdata, t(args))
-    }
+		argnames <- names(formals(fitnlme))
+		args <- setNames(vector('integer', length=length(argnames)), argnames)
+    args <- args[!match(argnames, names(newdata), 0)]
+    newdata <- data.frame(newdata, t(args))
     class(object) <- class(object)[class(object) != 'sitar']
     pred <- predict(object=object, newdata=newdata, level=level, ...)
-    attributes(pred) <- NULL
-    pred
+    if (deriv == 0) {
+      pred <- yfun(pred)
+      attributes(pred) <- NULL
+      return(pred)
+    }
+# deriv = 1
+    if (level == 1) pred <- predict(object=object, newdata=newdata, level=0, ...)
+# level 0 velocity predictions
+    vel0 <- predict(makess(x, pred, xfun=xfun, yfun=yfun), xfun(x), deriv=1)
+    if (level == 0) return(vel0$y)
+# level 1 velocity predictions
+    vel1 <- spline(vel0, method='natural', xout=xfun(xyadj(object, newdata)$x))
+    re <- ranef(object)
+    if (!is.null(re$c)) vel1$y <- vel1$y * exp(re$c[id])
+    return(vel1$y)
   }
 
   getData.sitar <- function(object) {
     object$call <- object$call.sitar
     class(object) <- 'lme'
     getData(object)
-  }
-
-  getVarCov.sitar <- function(obj, ...) {
-    class(obj) <- 'lme'
-    getVarCov(obj)
   }
