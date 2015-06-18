190a12edebb27b115cd6a21b78192319cf5f523b
Tim Cole <tim.cole@ucl.ac.uk>
2015-06-18 21:30:16 +0100

    predict.sitar look for x and id in newdata

--- R/predict.R
+++ R/predict.R
@@ -1,10 +1,12 @@
   predict.sitar <- function(object, newdata, level=1, ..., deriv=0, xfun=I, yfun=I) {
 # create x and id variables in newdata
+    if (missing(newdata)) newdata <- getData(object)
     oc <- object$call.sitar
-    if (missing(newdata)) newdata <- eval(oc$data)
-    newdata$x <- x <- eval(oc$x, newdata)
-    newdata$id <- id <- if (any(level == 1)) eval(oc$id, newdata) else
-      rep.int(getGroups(object)[1], nrow(newdata))
+    if (!is.null(newdata$x)) x <- newdata$x else
+      newdata$x <- x <- eval(oc$x, newdata)
+    if (!is.null(newdata$id)) id <- newdata$id else
+      newdata$id <- id <- if (any(level == 1)) eval(oc$id, newdata) else
+        rep.int(getGroups(object)[1], nrow(newdata))
 # attach object for fitnlme
     on.exit(detach(object))
     eval(parse(text='attach(object)'))
@@ -15,7 +17,7 @@
 		args <- setNames(vector('integer', length=length(argnames)), argnames)
     args <- args[!match(argnames, names(newdata), 0)]
     newdata <- data.frame(newdata, t(args))
-    class(object) <- class(object)[class(object) != 'sitar']
+    class(object) <- class(object)[-1]
     pred <- predict(object=object, newdata=newdata, level=level, ...)
     if (deriv == 0) {
       pred <- yfun(pred)
@@ -28,9 +30,9 @@
     vel0 <- predict(makess(x, pred, xfun=xfun, yfun=yfun), xfun(x), deriv=1)
     if (level == 0) return(vel0$y)
 # level 1 velocity predictions
-    vel1 <- spline(vel0, method='natural', xout=xfun(xyadj(object, newdata)$x))
-    re <- ranef(object)
-    if (!is.null(re$c)) vel1$y <- vel1$y * exp(re$c[id])
+    vel1 <- spline(vel0, method='natural',
+                   xout=xfun(xyadj(x=x, id=id, object=object)$x))
+    if (!is.null(c <- ranef(object)$c)) vel1$y <- vel1$y * exp(c[id])
     return(vel1$y)
   }
 
