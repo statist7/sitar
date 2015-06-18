8220e67a536a2a3a32eddd08659002a6d2a87e3a
Tim Cole <tim.cole@ucl.ac.uk>
2015-06-18 21:33:24 +0100

    plot.sitar tidy code and simplify with xyadj

--- R/plot.sitar.R
+++ R/plot.sitar.R
@@ -1,4 +1,4 @@
-	plot.sitar <- function(x, opt="dv", labels, apv=FALSE, xfun, yfun, subset=NULL, abc=c(a=0, b=0, c=0), add=FALSE, nlme=FALSE, ...)
+	plot.sitar <- function(x, opt="dv", labels, apv=FALSE, xfun=I, yfun=I, subset=NULL, abc=c(a=0, b=0, c=0), add=FALSE, nlme=FALSE, ...)
 #	plot curves from sitar model
 #	opt:
 #		d = fitted distance curve (labels[1] = x, labels[2] = y)
@@ -34,24 +34,18 @@
     seq(rx[1], rx[2], length.out=nx)
   }
 
-	model <- x
-	rm(x)
 	if (nlme) {
-		mcall <- match.call()[-1]
-		names(mcall)[[1]] <- 'x'
-		do.call('plot.lme', as.list(mcall))
+		do.call('plot.lme', as.list(match.call()[-1]))
 	}
 	else {
+	  model <- x
+		data <- getData(model)
 		mcall <- model$call.sitar
-		data <- eval(mcall$data, parent.frame())
-#	subset used to fit model
-		subset <- eval(mcall$subset, data)
-		if (!is.null(subset)) data <- data[subset, ]
 		x <- eval(mcall$x, data)
 		y <- eval(mcall$y, data)
-		id <- factor(eval(mcall$id, data)) # factor added 23/4/13
+		id <- factor(eval(mcall$id, data))
 		nf <- length(fitted(model))
-		if (nf != length(y)) stop(paste('model (length=', nf, ') incompatible with data (rows=', length(y), ')', sep=''))
+		if (nf != length(y)) stop(paste0('model (length=', nf, ') incompatible with data (rows=', length(y), ')'))
 
 #	extract list(...)
 		ccall <- match.call()[-1]
@@ -74,11 +68,34 @@
 			ARG <- c(ARG, list(ylab=yl))
 		}
 		else yl <- ARG$ylab
+# if labels not specified create it
+		if (missing(labels)) labels <- c(xl, yl, paste(yl, 'velocity'))
+		# if (missing(labels)) labels <- c(xl, yl, ifelse(typeof(yl) == 'expression', expression(paste(as.character(yl), '~~velocity')), paste(yl, 'velocity')))
 
 #	create output list
 		xy <- list()
 
-#	plot fitted distance and velocity curves
+		#	plot y vs t by subject
+		if (grepl("u", opt)) {
+		  xt <- x
+		  yt <- y
+		  if (!missing(xfun)) xt <- xfun(xt)
+		  if (!missing(yfun)) yt <- yfun(yt)
+		  do.call("mplot", c(list(x=xt, y=yt, id=id, subset=subset, add=add), ARG))
+		  add <- TRUE
+		}
+
+		#	plot fitted curves by subject
+		if (grepl("f", opt)) {
+		  xt <- x
+		  yt <- fitted(model, level=1)
+		  if (!missing(xfun)) xt <- xfun(xt)
+		  if (!missing(yfun)) yt <- yfun(yt)
+		  do.call("mplot", c(list(x=xt, y=yt, id=id, subset=subset, add=add), ARG))
+		  add <- TRUE
+		}
+
+		#	plot fitted distance and velocity curves
 		if (grepl("d", opt) || grepl("v", opt) || apv) {
 			xt <- xseq(x[subset])
   		newdata <- data.frame(x=xt)
@@ -93,45 +110,45 @@
         }
      }
 			yt <- predict(object=model, newdata=newdata, level=0)
+			vt <- predict(object=model, newdata=newdata, level=0, deriv=1, xfun=xfun, yfun=yfun)
+#	derive cubic smoothing spline curve
+			xy$ss <- ss <- makess(xt, yt, xfun=xfun, yfun=yfun)
+
 #	adjust for abc
 			if (!missing(abc)) {
 #	if abc is named ensure 3 values match model random effects
 				if (!is.null(names(abc))) {
-					random <- dimnames(ranef(model))[[2]]
+					random <- names(ranef(model))
 					for (l in letters[1:3])
 						if (is.na(abc[l]) || !l %in% random) abc[l] <- 0
+					abc <- data.frame(as.list(abc))
 				}
 				else
 #	else abc is id level
 				if (length(abc) == 1) {
-					idabc <- dimnames(ranef(model))[[1]] %in% abc
+					idabc <- rownames(ranef(model)) %in% abc
 					if (sum(idabc) == 0) stop(paste('id', abc, 'not found'))
-					abc <- ranef(model)[idabc,]
+					abc <- ranef(model)[idabc, ]
 				}
 				else stop('abc should be either single id level or up to three named random effect values')
-				xoffset <- model$xoffset
-				if (is.null(xoffset)) xoffset <- 0
-				if (!is.na(fixef(model)['b'])) xoffset <- xoffset + fixef(model)['b']
-				xt <- (xt - xoffset) / exp(abc[['c']]) + xoffset + abc[['b']]
-				yt <- yt + abc[['a']]
+			  yt <- xyadj(xt, yt, id, model, abc=abc, tomean=FALSE)
+			  xt <- yt$x
+			  yt <- yt$y
 			}
-#	derive cubic smoothing spline curve
-			xy$ss <- ss <- makess(xt, yt, xfun=xfun, yfun=yfun)
-			ss1 <- predict(ss, deriv=1)
-			ss2 <- predict(ss, deriv=2)
-			if (missing(labels)) labels <- c(xl, yl, paste(yl, 'velocity'))
-			# if (missing(labels)) labels <- c(xl, yl, ifelse(typeof(yl) == 'expression', expression(paste(as.character(yl), '~~velocity')), paste(yl, 'velocity')))
+
+			if (!missing(xfun)) xt <- xfun(xt)
+			if (!missing(yfun)) yt <- yfun(yt)
 			if (grepl("d", opt) && grepl("v", opt)) {
-				xy <- do.call("y2plot", c(list(x=ss$x, y1=ss$y, y2=ss1$y, labels=labels, add=add, xy=xy), ARG))
+				xy <- do.call("y2plot", c(list(x=xt, y1=yt, y2=vt, labels=labels, add=add, xy=xy), ARG))
 				add <- TRUE
 			} else
 			if (grepl("d", opt)) {
-				xy <- do.call("y2plot", c(list(x=ss$x, y1=ss$y, add=add, xy=xy), ARG))
+				xy <- do.call("y2plot", c(list(x=xt, y1=yt, add=add, xy=xy), ARG))
 				add <- TRUE
 			} else
 			if (grepl("v", opt)) {
 				ARG$ylab <- labels[3]
-				xy <- do.call("y2plot", c(list(x=ss$x, y1=ss1$y, add=add, xy=xy), ARG))
+				xy <- do.call("y2plot", c(list(x=xt, y1=vt, add=add, xy=xy), ARG))
 				add <- TRUE
 			}
 		}
@@ -147,31 +164,14 @@
 			add <- TRUE
 		}
 
-#	plot y vs t by subject
-		if (grepl("u", opt)) {
-			if (!missing(xfun)) x <- xfun(x)
-			if (!missing(yfun)) y <- yfun(y)
-  		do.call("mplot", c(list(x=x, y=y, id=id, subset=subset, add=add), ARG))
-			add <- TRUE
-		}
-
-#	plot fitted curves by subject
-		if (grepl("f", opt)) {
-			y <- fitted(model, level=1)
-			if (!missing(xfun)) x <- xfun(x)
-			if (!missing(yfun)) y <- yfun(y)
-    	do.call("mplot", c(list(x=x, y=y, id=id, subset=subset, add=add), ARG))
-			add <- TRUE
-		}
-
 #	plot adjusted y vs adjusted t by subject
 		if (grepl("a", opt)) {
-			y <- xyadj(model)
-			x <- y$x
-			y <- y$y
-			if (!missing(xfun)) x <- xfun(x)
-			if (!missing(yfun)) y <- yfun(y)
-    	do.call("mplot", c(list(x=x, y=y, id=id, subset=subset, add=add), ARG))
+			yt <- xyadj(x, y, id, model)
+			xt <- yt$x
+			yt <- yt$y
+			if (!missing(xfun)) xt <- xfun(xt)
+			if (!missing(yfun)) yt <- yfun(yt)
+    	do.call("mplot", c(list(x=xt, y=yt, id=id, subset=subset, add=add), ARG))
 			add <- TRUE
 		}
 #	plot vertical line at age of peak velocity
