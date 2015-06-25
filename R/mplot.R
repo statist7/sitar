f6af9fde38e9383e33a2817c91bf190d95d6a479
Tim Cole <tim.cole@ucl.ac.uk>
2015-06-25 09:50:12 +0100

    mplot include bg and cex in lists

--- R/mplot.R
+++ R/mplot.R
@@ -3,7 +3,7 @@
 #	x and y can be name or character
 #	subset defines a subset of rows
 #	add TRUE suppresses plot axes
-#	... parameters where col, lty, lwd, pch can depend on id
+#	... parameters where bg, cex, col, lty, lwd, pch can depend on id
 
 #	save x y id
 	mcall <- match.call()[-1]
@@ -17,7 +17,7 @@
   	})
 	}
 
-#	extract and save vector par args: col lty lwd pch
+#	extract and save vector par args: bg cex col lty lwd pch
 	if (length(dots <- match.call(expand.dots=FALSE)$...) > 0) {
 		ARG <- lapply(as.list(dots), eval, envir = data, enclos = parent.frame())
 		cnames <- names(ARG)[lapply(ARG, length) == nrow(df)]
