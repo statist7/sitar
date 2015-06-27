d9c5774b3fe4202673148d14280be1201360d192
Tim Cole <tim.cole@ucl.ac.uk>
2015-06-27 22:21:11 +0100

    predict.sitar add abc arg

--- R/predict.sitar.R
+++ R/predict.sitar.R
@@ -1,4 +1,7 @@
-  predict.sitar <- function(object, newdata, level=1, ..., deriv=0, xfun=I, yfun=I) {
+  predict.sitar <- function(object, newdata, level=1, ..., deriv=0,
+                            abc=ranef(object), xfun=I, yfun=I) {
+# function expand
+    expand <- function(z) if (is.null(z)) 0 else if (nrow(abc) == 1) z else z[id]
 # create x and id variables in newdata
     if (missing(newdata)) newdata <- getData(object)
     oc <- object$call.sitar
@@ -16,24 +19,46 @@
 		argnames <- names(formals(fitnlme))
 		args <- setNames(vector('integer', length=length(argnames)), argnames)
     args <- args[!match(argnames, names(newdata), 0)]
+# add other fitnlme args to newdata
     newdata <- data.frame(newdata, t(args))
+# set class to nlme
     class(object) <- class(object)[-1]
-    pred <- predict(object=object, newdata=newdata, level=level, ...)
-    if (deriv == 0) {
-      pred <- yfun(pred)
-      attributes(pred) <- NULL
-      return(pred)
+# simple prediction
+    if (deriv == 0 && missing(abc)) {
+      pred <- yfun(predict(object=object, newdata=newdata, level=level, ...))
     }
-# deriv = 1
-    if (level == 1) pred <- predict(object=object, newdata=newdata, level=0, ...)
-# level 0 velocity predictions
-    vel0 <- predict(makess(x, pred, xfun=xfun, yfun=yfun), xfun(x), deriv=1)
-    if (level == 0) return(vel0$y)
-# level 1 velocity predictions
-    vel1 <- spline(vel0, method='natural',
-                   xout=xfun(xyadj(x=x, id=id, object=object)$x))
-    if (!is.null(c <- ranef(object)$c)) vel1$y <- vel1$y * exp(c[id])
-    return(vel1$y)
+# complex prediction
+    else {
+# mean distance curve
+      pred <- predict(object=object, newdata=newdata, level=0, ...)
+# DISTANCE
+      if (deriv == 0) {
+# mean distance curve back-transformed
+        if (level == 0) pred <- yfun(pred)
+# level 1 prediction based on x changed to reflect individual b and c
+        else {
+          pred <- spline(list(x=x, y=pred), method='natural',
+                         xout=xyadj(x=x, id=id, object=object, abc=abc)$x)$y
+# add individual a to prediction
+          if (!is.null(ranef(object)$a)) pred <- yfun(pred + expand(abc$a))
+        }
+      }
+# VELOCITY
+      else {
+# mean velocity curve on back-transformed axes
+        vel0 <- predict(makess(x, pred, xfun=xfun, yfun=yfun), xfun(x), deriv=1)
+        if (level == 0) pred <- vel0$y
+# level 1 prediction based on x changed to reflect individual b and c
+        else {
+          pred <- spline(vel0, method='natural',
+                         xout=xfun(xyadj(x=x, id=id, object=object, abc=abc)$x))$y
+# multiply velocity by individual c (inexact when xfun != I)
+          if (!is.null(ranef(object)$c)) pred <- pred * exp(expand(abc$c))
+        }
+      }
+    }
+    attributes(pred) <- NULL
+    return(pred)
   }
 
   getData.sitar <- function(object) {
