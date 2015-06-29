bbf598c62442ef8255142f2ebe8fd4b2c940a620
Tim Cole <tim.cole@ucl.ac.uk>
2015-06-29 23:04:21 +0100

    xyadj tidy up abc

--- R/xyadj.R
+++ R/xyadj.R
@@ -1,9 +1,12 @@
-xyadj <- function(x, y=NULL, id, object, abc=ranef(object), tomean=TRUE) {
+xyadj <- function(x, y=NULL, id, object, abc=NULL, tomean=TRUE) {
 #	returns x and y adjusted for random effects a, b and c
+  re <- ranef(object)
+  if (is.null(abc)) abc <- re
+  abc[, letters[1:3][!letters[1:3] %in% names(abc)]] <- 0 # fill with zeros
+  abc[, letters[1:3][!letters[1:3] %in% names(re)]] <- 0 # omit not in model
+  abc <- abc[id, ]
   if (is.null(xoffset <- object$xoffset)) xoffset <- 0
   if (!is.na(b0 <- fixef(object)['b'])) xoffset <- xoffset + b0
-  if (nrow(abc) > 1) abc <- abc[id, ]
-  abc[, letters[1:3][!letters[1:3] %in% names(abc)]] <- 0
   if (tomean) {
     x.adj <- (x - xoffset - abc$b) * exp(abc$c) + xoffset
     y.adj <- y - abc$a
