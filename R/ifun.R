39959c413a1b7ff7981b6587179e7d5ba6dfc69a
Tim Cole <tim.cole@ucl.ac.uk>
2015-08-05 18:23:30 +0100

    ifun use all.vars and nvars to handle numeric language terms

--- R/ifun.R
+++ R/ifun.R
@@ -3,17 +3,16 @@ ifun <- function(fun) {
 ###########################
   recur <- function(fun, funinv=quote(x)) {
     fun <- as.expression(fun)[[1]]
-    if (is.numeric(fun)) stop('expression should contain just one name')
 #   if bracketed drop brackets
     while (length(fun) > 1 && fun[[1]] == as.name('(')) fun <- fun[[2]]
     if ((ne <- length(fun)) > 1) {
-#     element of expression containing language (either 2 or 3, ignoring leading symbol)
-      x1 <- which(vapply(fun, function(f) is.language(f) && f != as.name('pi'), TRUE))[-1]
-      if (length(x1) != 1) stop('expression should contain just one name')
-#     element containing numeric (only relevant when ne == 3)
+#     element of expression containing varname (either 2 or 3, ignoring leading symbol)
+      x1 <- which(vapply(fun, function(f) nvars(f) == 1, TRUE))[-1]
+#     element not containing varname (only relevant when ne == 3)
       n1 <- 5 - x1
 #     expressions matching leading symbol with same length and x arg position
-      nf <- which(vapply(fns, function(f) f[[1]] == fun[[1]] && length(f) == length(fun) && f[[x1]] == quote(x), TRUE))
+      nf <- which(vapply(fns, function(f)
+        f[[1]] == fun[[1]] && length(f) == length(fun) && f[[x1]] == 'x', TRUE))
       if (length(nf) == 0) stop (paste('unrecognised name:', deparse(fun[[1]])))
 #     if multiple matches check if numeric args are equal
       if (length(nf) > 1 && ne == 3) {
@@ -25,14 +24,14 @@ ifun <- function(fun) {
 #     use complement of pair as inverse function
       fn2 <- if (nf %% 2) fns[[nf + 1]] else fns[[nf - 1]]
 #     identify position of x arg in inverse function
-      x2 <- which(as.list(fn2) == quote(x))
+      x2 <- which(as.list(fn2) == 'x')
 #     if length 3 copy n arg
       if (length(fn2) == 3) {
         n2 <- 5 - x2
 #       function returns value for n arg
         f <- function(n) {}
         body(f) <- fn2[[n2]]
-        fn2[[n2]] <- f(fun[[n1]])
+        fn2[[n2]] <- f(eval(fun[[n1]]))
       }
 #     copy x from current inverse function
       fn2[[x2]] <- funinv
@@ -46,6 +45,10 @@ ifun <- function(fun) {
     return(list(fn=funinv, varname=fun))
   }
 ###########################
+# number of names in function ignoring pi
+  nvars <- function(fun) length((av <- all.vars(fun, unique=FALSE))[grep('pi', av, invert=TRUE)])
+###########################
+  if (nvars(fun) != 1) stop('expression should contain just one instance of one name')
   fns <- quote(c(x+n, x-n, x*n, x/n, x^n, x^(1/n), sqrt(x), x^2, exp(x), log(x),
                  expm1(x), log1p(x), n^x, log(x,n), log10(x), 10^x, log2(x),
                  2^x, n+x, x-n, n-x, n-x, n*x, x/n, n/x, n/x, +x, +x, -x, -x,
