bccfc4e377517e4b94c4c1d9f28735ec7e682725
Tim Cole <tim.cole@ucl.ac.uk>
2016-05-03 22:38:17 +0100

    predict major updates fixing Costanza's bugs
    shift abc code from xyadj
    create newdata reflecting covariates and factors
    tidy up level/deriv/abcset structure

--- R/predict.sitar.R
+++ R/predict.sitar.R
@@ -1,51 +1,57 @@
-  predict.sitar <- function(object, newdata, level=1, ..., deriv=0,
-                            abc=NULL, xfun=I, yfun=I) {
-# function expand
-    expand <- function(z) if (is.null(z)) 0 else z[id]
+  predict.sitar <- function(object, newdata=getData(object), level=1, ...,
+                            deriv=0, abc=ranef(object), xfun=I, yfun=I) {
 # create x and id variables in newdata
-    if (missing(newdata)) newdata <- getData(object)
     oc <- object$call.sitar
     if (is.null(newdata$x)) newdata$x <- eval(oc$x, newdata)
     x <- newdata$x
-    xoffset <- object$xoffset
-    if (is.null(xoffset)) {
+    if (is.null(xoffset <- object$xoffset)) {
       xoffset <- mean(getCovariate(object))
       warning('xoffset set to mean(x) - best to refit model')
     }
     newdata$x <- newdata$x - xoffset
+# create id in newdata
+    if (any(level == 1)) newdata$id <- eval(oc$id, newdata)
+    else newdata$id <- rep.int(getGroups(object)[1], nrow(newdata))
+    id <- newdata$id <- factor(newdata$id)
 # check abc
-    if (!is.null(abc)) {
-      abcset <- TRUE
-      if (!'data.frame' %in% class(abc)) abc <- data.frame(t(abc))
-      if (nrow(abc) == 1) level <- 0
-        else if (nrow(abc) == nrow(ranef(object))) abcset <- FALSE
-                                   else stop('abc not a variate of length up to 3')
+    if (abcset <- !is.data.frame(abc)) {
+      abc <- data.frame(t(abc))
+      abc[, letters[1:3][!letters[1:3] %in% names(abc)]] <- 0 # fill with zeros
+      level <- 0
+      id <- rep.int(1, length(x))
     }
-    else {
-      abcset <- FALSE
-      abc <- ranef(object)
-    }
-# check for id in newdata
-    if (id.ok <- deparse(oc$id) %in% names(newdata)) newdata$id <- factor(eval(oc$id, newdata))
-    else newdata$id <- factor(rep.int(getGroups(object)[1], nrow(newdata)))
-    id <- factor(newdata$id)
+    abc[, letters[1:3][!letters[1:3] %in% names(ranef(object))]] <- 0 # omit not in model
+    abc <- abc[id, ]
 # check if old-style object lacking fitnlme
     if(!'fitnlme' %in% names(object)) {
-      cat('need to update object to obtain fitnlme\n')
+      warning('fitnlme missing - best to refit model')
       object <- update(object, control=nlmeControl(maxIter=0, pnlsMaxIter=0, msMaxIter=0))
     }
 # attach object for fitnlme
     on.exit(detach(object))
     eval(parse(text='attach(object)'))
-# identify extra covariates needed in newdata, omitting x and fixed effects
+# identify covariates in newdata other than x and id
+		covnames <- names(newdata)
+		covnames <- covnames[!covnames %in% c('x', 'id')]
+# identify covariates needed in newdata, omitting x and fixed effects, and set them to 0
 		argnames <- names(formals(fitnlme))
-		argnames <- argnames[!match(argnames, names(fixef(object)), 0)][-1]
-# zero or centre each covariate
-		if (length(argnames) > 0) {
-		  if (any(match(argnames, names(newdata), 0))) gd <- getData(object)
-		  for (i in argnames) {
-		    if (match(i, names(newdata), 0)) newdata[[i]] <- newdata[[i]] - mean(gd[[i]])
-		    else newdata[[i]] <- 0
+		argnames <- argnames[!argnames %in% names(fixef(object))][-1]
+		newdata[, argnames] <- 0
+# centre each named covariate
+		covnames <- covnames[covnames %in% argnames]
+		if (length(covnames) > 0) {
+		  gd <- getData(object)
+		  for (i in covnames) {
+# continuous variable
+		    if (i %in% argnames) newdata[[i]] <- newdata[[i]] - mean(gd[[i]])
+	      else {
+# factor as instrumental variable(s)
+	        lev <- levels(gd[, i])
+	        for (j in 2:length(lev)) {
+	          k <- paste0(i, lev[j])
+	          newdata[[k]] <- as.numeric(newdata[[i]] == lev[j]) - mean(gd[[i]] == lev[j])
+	        }
+	      }
 		  }
 		}
 # set class to nlme
@@ -55,36 +61,34 @@
       pred <- yfun(predict(object=object, newdata=newdata, level=level, ...))
     }
 # complex prediction
-    else {
+    else { # deriv == 1 || abcset
 # mean distance curve
       pred <- predict(object=object, newdata=newdata, level=0, ...)
 # DISTANCE
-      if (deriv == 0) {
-# mean distance curve back-transformed
-        if (level == 0 && !abcset) pred <- yfun(pred)
+      if (deriv == 0) { # abcset
 # level 1 prediction based on x changed to reflect individual b and c
-        else {
-          pred <- spline(list(x=x, y=pred), method='natural',
-                         xout=xyadj(x=x, id=id, object=object, abc=abc)$x)$y
+        pred <- spline(list(x=x, y=pred), method='natural',
+                      xout=xyadj(x=x, id=id, object=object, abc=abc)$x)$y
 # add individual a to prediction
-          if (!is.null(ranef(object)$a)) pred <- yfun(pred + expand(abc$a))
-        }
+        if (!is.null(abc$a)) pred <- yfun(pred + abc$a)
       }
 # VELOCITY
-      else {
+      else { # deriv == 1
 # mean velocity curve on back-transformed axes
         vel0 <- predict(makess(x, pred, xfun=xfun, yfun=yfun), xfun(x), deriv=1)
         if (level == 0 && !abcset) pred <- vel0$y
+        else { # level == 1 || abcset
 # level 1 prediction based on x changed to reflect individual b and c
-        else {
           pred <- spline(vel0, method='natural',
                          xout=xfun(xyadj(x=x, id=id, object=object, abc=abc)$x))$y
 # multiply velocity by individual c (inexact when xfun != I)
-          if (!is.null(ranef(object)$c)) pred <- pred * exp(expand(abc$c))
+          if (!is.null(abc$c)) pred <- pred * exp(abc$c)
         }
       }
+      attr(pred, 'label') <- 'Predicted values'
     }
-    if (id.ok && is.null(dim(pred))) names(pred) <- id
+    asList <- list(...)$asList
+    if (level == 1 && !is.null(asList) && !asList) names(pred) <- id
     return(pred)
   }
 
@@ -99,7 +103,7 @@
     getVarCov(obj)
   }
 
-  getCovariate.sitar <- function (object, ...)
+  getCovariate.sitar <- function(object, ...)
   {
     eval(object$call.sitar$x, getData(object))
   }
