95982ef273e4160f40578ca549b3279668c7e13f
Tim Cole <tim.cole@ucl.ac.uk>
2016-05-03 22:44:07 +0100

    xyadj abc code shifted to predict

--- R/xyadj.R
+++ R/xyadj.R
@@ -1,11 +1,7 @@
-xyadj <- function(x, y=NULL, id, object, abc=NULL, tomean=TRUE) {
+xyadj <- function(x, y=NULL, id, object, abc=ranef(object)[id, ], tomean=TRUE) {
 #	returns x and y adjusted for random effects a, b and c
-  re <- ranef(object)
-  if (is.null(abc)) abc <- re
-  abc[, letters[1:3][!letters[1:3] %in% names(abc)]] <- 0 # fill with zeros
-  abc[, letters[1:3][!letters[1:3] %in% names(re)]] <- 0 # omit not in model
-  abc <- abc[id, ]
-  if (is.null(xoffset <- object$xoffset)) xoffset <- mean(getCovariate(object))
+  abc[, letters[1:3][!letters[1:3] %in% names(ranef(object))]] <- 0 # omit not in model
+  xoffset <- object$xoffset
   if (!is.na(b0 <- fixef(object)['b'])) xoffset <- xoffset + b0
   if (tomean) {
     x.adj <- (x - xoffset - abc$b) * exp(abc$c) + xoffset
