e72569dbf685a5e5b9b873beee757c5a1442ec8b
Tim Cole <tim.cole@ucl.ac.uk>
2019-01-02 22:45:39 +0000

    predict.sitar major rewrite to simplify code, fix abc, and improve prediction of individual velocity curves

--- R/predict.sitar.R
+++ R/predict.sitar.R
@@ -9,17 +9,17 @@
 #'
 #' @param object an object inheriting from class \code{sitar}.
 #' @param newdata an optional data frame to be used for obtaining the
-#' predictions. It requires named columns for \code{x}, and for \code{id} if
+#' predictions, defaulting to the data used to fit \code{object}.
+#' It requires named columns for \code{x}, and for \code{id} if
 #' \code{level = 1}, matching the names in \code{object}. Variables with the
 #' reserved names \code{x=.x} or \code{id=.id} take precedence over the model
 #' \code{x} and \code{id} variables. Any covariates in
 #' \code{a.formula}, \code{b.formula} or \code{c.formula} can also be included.
 #' By default their values are set to the mean, so when \code{level = 0} the
 #' prediction represents the mean curve.
-#'
-#' @param level an optional integer giving the level(s) of grouping to be used
+#' @param level an optional integer vector giving the level(s) of grouping to be used
 #' in obtaining the predictions, level 0 corresponding to the population
-#' predictions. Defaults to level 1.
+#' predictions. Defaults to level 1, and \code{level = 0:1} fits both levels.
 #' @param \dots other optional arguments: \code{asList}, \code{na.action} and
 #' \code{naPattern}.
 #' @param deriv an optional integer specifying predictions corresponding to
@@ -27,10 +27,10 @@
 #' specifies the distance curve, \code{deriv = 1} the velocity curve and
 #' \code{deriv = 2} the acceleration curve.
 #' @param abc an optional named vector containing values of a subset of
-#' \code{a}, \code{b} and \code{c}, default \code{NULL}. If \code{abc} is set,
-#' \code{level} is set to 0. It gives predictions for a single subject with the
+#' \code{a}, \code{b} and \code{c}, default \code{NULL}. Ignored if
+#' \code{level = 0}. It gives predictions for a single subject with the
 #' specified values of \code{a}, \code{b} and \code{c}, where missing values
-#' are set to 0.
+#' are set to 0. Alternatively \code{abc} can contain the value for a single id.
 #' @param xfun an optional function to apply to \code{x} to convert it back to
 #' the original scale, e.g. if x = log(age) then xfun = exp. Only relevant if
 #' \code{deriv > 0} - see Details.
@@ -60,8 +60,8 @@
 #' mplot(age, vel1, id, heights, col=id, add=TRUE)
 #'
 #' @export
-  predict.sitar <- function(object, newdata=getData(object), level=1, ...,
-                            deriv=0, abc=NULL,
+  predict.sitar <- function(object, newdata=getData(object), level=1L, ...,
+                            deriv=0L, abc=NULL,
                             xfun=function(x) x, yfun=function(y) y) {
 # create x in newdata
     oc <- object$call.sitar
@@ -74,41 +74,42 @@
       warning('xoffset set to mean(x) - best to refit model')
     }
     newdata$x <- x - xoffset
-# create id in newdata
-    id <- if ('.id' %in% names(newdata))
-      newdata$.id
-    else {
-      if (any(level == 1) && missing(abc))
-        eval(oc$id, newdata)
-      else
-        rep.int(getGroups(object)[1], nrow(newdata))
-    }
-    newdata$id <- id
-# check abc as length-3 vector or 1-row data frame
-    if (is.null(abc))
-      abc <- ranef(object)
-    else if (is.vector(abc)) {
-#	abc is named vector
-      if (!is.null(names(abc)) && all(unique(names(abc)) %in% letters[1:3])) {
-        abc <- data.frame(t(abc))
+# check abc as id
+    id <- rownames(ranef(object))
+    if (all(level == 0L))
+      abc <- NULL
+    else
+    if (!is.null(abc)) {
+      if (is.null(names(abc))) {
+        if (is.vector(abc) && length(abc) == 1 && as.character(abc) %in% id) {
+          newdata$id <- abc
+          abc <- NULL
+        }
+        else
+          stop('abc unrecognised as id')
+      } else {
+# check abc as length 3 vector or data frame
+        if (is.vector(abc))
+          abc <- data.frame(t(abc))
+        if (!all(names(abc) %in% letters[1:3]))
+          stop('abc unrecognised as data frame')
         abc[, letters[1:3][!letters[1:3] %in% names(abc)]] <- 0 # fill with zeros
-      } else
-#	abc is id level
-      if (length(abc) == 1) {
-        . <- rownames(ranef(object)) %in% abc
-        if (!any(.))
-          stop(paste('id', abc, 'not found'))
-        abc <- ranef(object)[., , drop=FALSE]
       }
     }
-    if (is.null(nrow(abc)))
-      stop('abc should be either a single id level or up to three named random effect values')
-    else if (abcset <- nrow(abc) == 1) {
-      level <- 0
-    } else
-      abc <- abc[id, , drop=FALSE]
+# create id in newdata
+    newdata$id <- if ('.id' %in% names(newdata))
+      newdata$.id
+    else {
+      if (any(level == 1L) && (is.null(abc) || nrow(abc) > 1))
+        eval(oc$id, newdata)
+      else
+        factor(1, labels=id[1])
+    }
+    id <- newdata$id
+    if (!is.null(abc))
+      abc <- abc[id, ] # expand with id
 # check if old-style object lacking fitnlme
-    if(!'fitnlme' %in% names(object)) {
+    if (!'fitnlme' %in% names(object)) {
       warning('fitnlme missing - best to refit model')
       object <- update(object, control=nlmeControl(maxIter=0, pnlsMaxIter=0, msMaxIter=0))
     }
@@ -146,8 +147,8 @@
     		  }
     		}
       }
-# newdata subsetted (in plot)
       else {
+# newdata subsetted (in plot)
         gd <- update(object, returndata=TRUE)
         if (length(subset) == nrow(gd))
           gd <- gd[subset, argnames, drop=FALSE]
@@ -162,47 +163,47 @@
 # ensure deriv integral
     deriv <- as.integer(deriv)
 # simple prediction
-    if (deriv == 0 && !abcset) {
-      pred <- yfun(predict(object=object, newdata=newdata, level=level, ...))
-      return(pred)
-    }
-# complex prediction
-    else { # deriv != 0 || abcset
-# mean distance curve
-      pred <- predict(object=object, newdata=newdata, level=0, ...)
-# x changed to reflect individual b and c
-      xout <- xyadj(object=object, x=x, id=id, abc=abc)$x
+    if (deriv == 0L && is.null(abc))
+      return(yfun(predict(object, newdata, level=level, ...)))
+# else deriv > 0L || !is.null(abc)
 # DISTANCE
-      if (deriv == 0) { # abcset
+# level 0 prediction
+    pred0 <- yfun(predict(object, newdata, level=0L))
+    xy.id <- xyadj(object, x=x, y=0, id=id, abc=abc)
+    if (deriv == 0L) { # abc set
 # level 1 prediction
-        pred <- spline(list(x=x, y=pred), method='natural', xout=xout)$y
-# add individual a to prediction (inexact when yfun != y)
-        if (as.logical(abc$a))
-          pred <- yfun(pred + abc$a)
-      }
-# VELOCITY
-      else { # deriv != 0
-# mean velocity curve on back-transformed axes
-        vel0 <- predict(smooth.spline(xfun(x), yfun(pred)), xfun(x), deriv=deriv)
-        if (any(level == 0) && !abcset)
-          pred0 <- pred <- vel0$y
-        if (any(level == 1) || abcset) {
-# level 1 prediction
-          pred <- spline(vel0, method='natural', xout=xfun(xout))$y
-# multiply velocity by individual c (inexact when xfun != x)
-          if (as.logical(abc$c))
-            pred <- pred * exp(abc$c)
-        }
-      }
-# return data frame if level 0:1
-      if (length(level) > 1)
-        return(data.frame(id=id, predict.fixed=pred0, predict.id=pred))
-# add names or split by id if level 1
-      if (level == 1) {
-        asList <- ifelse(is.null(asList <- list(...)$asList), FALSE, asList)
-        if (asList) pred <- split(pred, id) else names(pred) <- id
-      }
-      attr(pred, 'label') <- 'Predicted values'
-      return(pred)
+      newdata$x <- xy.id$x - xoffset
+      pred <- yfun(predict(object, newdata, level=0L) - xy.id$y)
     }
+    else { # deriv > 0
+# VELOCITY
+# level 0 prediction
+      vel0 <- predict(smooth.spline(xfun(x), pred0), xfun(x), deriv=deriv)
+      pred <- vel0$y
+# velocity curve on back-transformed axes
+      if (any(level == 1L)) {
+# level 1 prediction
+        xout <- xy.id$x
+        vel <- spline(vel0, method='natural', xout=xfun(xout))$y
+        if (is.null(abc))
+          abc <- ranef(object)[id, , drop=FALSE]
+        if (!is.null(abc$c))
+          vel <- vel * exp(abc$c)
+        pred <- vel
+      }
+      pred0 <- vel0$y
+    }
+# return data frame if level 0:1
+    if (length(level) > 1)
+      return(data.frame(id=id, predict.fixed=pred0, predict.id=pred))
+# add names or split by id if level 1
+    if (level == 1L) {
+      asList <- ifelse(is.null(asList <- list(...)$asList), FALSE, asList)
+      if (asList)
+        pred <- split(pred, id)
+      else
+        names(pred) <- id
+    }
+    attr(pred, 'label') <- 'Predicted values'
+    return(pred)
   }
