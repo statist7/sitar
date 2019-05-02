936ca6ef1d44fbf01ff1f5e876bee8516cc3de5e
Tim Cole <tim.cole@ucl.ac.uk>
2019-05-02 18:30:52 +0100

    getPeakTakeoff algorithm now based on 3 points not 5

--- R/getPeakTakeoff.R
+++ R/getPeakTakeoff.R
@@ -1,10 +1,11 @@
 #' Identify peak or takeoff point of velocity curve
 #'
-#' Given vectors \code{x} and  \code{y}, returns their values at the (highest)
+#' Given vectors \code{x} and \code{y}, returns their values at the (highest)
 #' peak or (lowest) takeoff point of the smooth (e.g. cubic spline) velocity
-#' curve \code{y ~ x}, where \code{Dy} is zero and \code{D2y} is
-#' negative or positive respectively. The takeoff point is required to occur
-#' before the peak.
+#' curve \code{y ~ x}.
+#'
+#' The takeoff point is defined as the lowest velocity before the peak, so if
+#' the curve has no peak there can be no takeoff.
 #'
 #' @param x vector.
 #' @param y vector.
@@ -16,49 +17,58 @@
 #' ## create mean height velocity curve
 #' data(heights)
 #' m1 <- sitar(age, height, id, heights, 4)
-#' ## plot it
+#' ## plot velocity curve
 #' plot(m1, 'v')
 #' ## mark peak and takeoff
 #' xy <- plot_v(m1)
-#' points(t(getPeak(xy)), las=1, pch=17)
+#' points(t(getPeak(xy)), pch=17)
 #' points(t(getTakeoff(xy)), pch=25, col=2, bg=2)
 #' @export getPeakTakeoff
 getPeakTakeoff <- function(x, y = NULL, peak = TRUE) {
-  #	returns values of x and y at peak/takeoff, i.e. where dy/dx=0
   xy <- xy.coords(x, y)
   # sort data and remove duplicates
-  xy <- unique(as.data.frame(xy[1:2])[order(xy$x),])
+  xy <- unique(as.data.frame(xy[1:2])[order(xy$x), ])
   x <- xy$x
   y <- xy$y
-  sign_change <- ifelse(peak, -1, +1)
-  tp <-
-    which(c(FALSE, diff(diff(y) / diff(x) > 0) == sign_change, FALSE)) # turning point(s)
+  # find peak
+  ddy <- diff(diff(y) > 0)
+  # turning point(s)
+  tp <- which(ddy == -1) + 1
+  # return if no peak
   if (length(tp) == 0)
     return(c(x = NA, y = NA))
-  # turning point(s) found
-  if (length(tp) > 1) {
-    # multiple turning points - find the best
-    best <- y[tp]
-    best <-
-      ifelse(peak, which.max(best), which.min(best)) # point of tp
-    tp <- tp[best]
+  # find highest peak
+  tp <- tp[which.max(y[tp])]
+  # find takeoff(s) earlier than peak
+  if (!peak) {
+    tp <- which(ddy[seq_len(tp)] == 1) + 1
+    # find lowest takeoff
+    tp <- tp[which.min(y[tp])]
   }
-  region <- max(1, tp - 2):min(tp + 2, length(x)) # region of tp
+  # return if no takeoff
+  if (length(tp) == 0)
+    return(c(x = NA, y = NA))
+  # region of tp
+  region <- (tp - 1):(tp + 1)
   x <- x[region]
   y <- y[region]
-  curve <- lm(y ~ poly(x, 2, raw = TRUE)) # quadratic in x
-  x <- -curve$coef[[2]] / curve$coef[[3]] / 2 # x at tp
-  y <- unname(predict(curve, data.frame(x = x))) # y at tp
+  # quadratic in x
+  curve <- lm(y ~ poly(x, 2, raw = TRUE))
+  # x and y at tp
+  x <- -curve$coef[[2]] / curve$coef[[3]] / 2
+  y <- unname(predict(curve, data.frame(x = x)))
   return(c(x = x, y = y))
 }
 #' @rdname getPeakTakeoff
 #' @export
 getPeak <- function(x, y = NULL, peak = TRUE) {
+
 }
 
 #' @rdname getPeakTakeoff
 #' @export
 getTakeoff <- function(x, y = NULL, peak = FALSE) {
+
 }
 
 body(getTakeoff) <- body(getPeak) <- body(getPeakTakeoff)
